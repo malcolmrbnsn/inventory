9078795b6a31123975daae827296388b98743e49
robthr <calumrobinson9@gmail.com>
2021-07-18 10:50:32 +1000

    fixed db vars, removed mail

--- helpers/testmail.js
+++
@@ -1,9 +0,0 @@
-const {boxAssignedEmail} = require("./mail");
-const {Seller} = require("../models")
-
-async function giga() {
-    let seller = await Seller.findOne({name: "malcolm"});
-boxAssignedEmail(seller)
-}
-
-giga()
